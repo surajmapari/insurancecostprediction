5ed685d91a2c1dbb641e628fe8c9fbffeed601a2
SURAJ MAPARI <57449206+surajmapari@users.noreply.github.com>
2023-01-31 22:44:11 +0530

    Add files via upload

---
+++ Online Reservtaion System/Main/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import onlinereservationsystem.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Login login = new Login();
+    }
+}
